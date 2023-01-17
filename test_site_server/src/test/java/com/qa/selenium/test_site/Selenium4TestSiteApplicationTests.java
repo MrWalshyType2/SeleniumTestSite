f67cf9b73f4553bf25809bc846e0c80a5ec7067e
MrWalshyType2 <mwalsh@qa.com>
2023-01-17 16:37:37 +0000

    Added server

---
+++ test_site_server/src/test/java/com/qa/selenium/test_site/Selenium4TestSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.selenium.test_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Selenium4TestSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
